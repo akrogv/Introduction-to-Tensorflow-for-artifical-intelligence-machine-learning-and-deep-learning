6abc00ae010cbcd61fdad635ef71df9f109747c7
gorkagamo <gogamoo@hotmail.com>
2022-06-10 16:51:59 +0200

    Explanation off Convolutions and Pooling. Introduction

--- Week3/Convolutions and pooling.py
+++ Week3/Convolutions and pooling.py
@@ -14,11 +14,16 @@ model = tf.keras.models.Sequential([
     tf.keras.layers.Conv2D(64, (3,3), activation = 'relu',input_shape=(28,28,1)),
     #LAYEER CONV2D. Creates 64 filters of 3x3 pixels, activated by relu (only positive values), input shape still is 28x28, 1 means color depth.
     tf.keras.layers.MaxPooling2D(2,2),
+
     tf.keras.layers.Conv2D(64,(3,3),activation='relu'),
     tf.keras.layers.MaxPooling2D(2,2),
+
     tf.keras.layers.Flatten(),
     tf.keras.layers.Dense(128,activation='relu'),
     tf.keras.layers.Dense(10,activation='softmax')
-)
+])
+
+model.summary() #inspect the layers and see the journey of the data.
+
 
 #https://bit.ly/2UGa7uH

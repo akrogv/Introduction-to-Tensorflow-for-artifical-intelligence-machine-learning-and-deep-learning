61edb581c2f836260ddbe1d86516eb4d7dd8089a
gorkagamo <gogamoo@hotmail.com>
2022-06-12 10:06:21 +0200

    Defined model

--- Week 4/Handling Complex Images.py
+++ Week 4/Handling Complex Images.py
@@ -1,1 +1,46 @@
-""
+
+import tensorflow as tf
+import os
+import zipfile
+from os import path, getcwd, chdir
+
+# DO NOT CHANGE THE LINE BELOW. If you are developing in a local
+# environment, then grab happy-or-sad.zip from the Coursera Jupyter Notebook
+# and place it inside a local folder and edit the path to that location
+path = f"{getcwd()}/../tmp2/happy-or-sad.zip"
+
+zip_ref = zipfile.ZipFile(path, 'r')
+zip_ref.extractall("/tmp/h-or-s")
+zip_ref.close()
+
+
+# GRADED FUNCTION: train_happy_sad_model
+def train_happy_sad_model():
+    # Please write your code only where you are indicated.
+    # please do not remove # model fitting inline comments.
+
+    DESIRED_ACCURACY = 0.999
+
+    class myCallback(tf.keras.callbacks.Callback):
+        # Your Code
+        def on_epoch_end(self, epoch, logs={}):
+            if (logs.get('acc') > DESIRED_ACCURACY):
+                print("\nReached 99.9% accuracy so cancelling training!")
+                self.model.stop_training = True
+
+    callbacks = myCallback()
+
+    # This Code Block should Define and Compile the Model. Please assume the images are 150 X 150 in your implementation.
+    model = tf.keras.models.Sequential([
+        # Your Code Here
+        tf.keras.layers.Conv2D(16, (3, 3), activation='relu', input_shape=(150, 150, 3)),
+        tf.keras.layers.MaxPooling2D(2, 2),
+        tf.keras.layers.Conv2D(32, (3, 3), activation='relu'),
+        tf.keras.layers.MaxPooling2D(2, 2),
+        tf.keras.layers.Conv2D(32, (3, 3), activation='relu'),
+        tf.keras.layers.MaxPooling2D(2, 2),
+        tf.keras.layers.Flatten(),
+        tf.keras.layers.Dense(512, activation='relu'),
+        tf.keras.layers.Dense(1, activation='sigmoid')
+    ])
+

4430bcab1d880f9a83f6edc6a6e9f6e3c99c9701
gorkagamo <gogamoo@hotmail.com>
2022-06-13 18:33:35 +0200

    Weekly assignment

--- Week 4/Handling Complex Images.py
+++ Week 4/Handling Complex Images.py
@@ -1,36 +1,75 @@
-
+import matplotlib.pyplot as plt
 import tensorflow as tf
+import numpy as np
 import os
-import zipfile
-from os import path, getcwd, chdir
 
-# DO NOT CHANGE THE LINE BELOW. If you are developing in a local
-# environment, then grab happy-or-sad.zip from the Coursera Jupyter Notebook
-# and place it inside a local folder and edit the path to that location
-path = f"{getcwd()}/../tmp2/happy-or-sad.zip"
+from tensorflow.keras.preprocessing.image import load_img
 
-zip_ref = zipfile.ZipFile(path, 'r')
-zip_ref.extractall("/tmp/h-or-s")
-zip_ref.close()
+base_dir = "./data/"
+happy_dir = os.path.join(base_dir, "happy/")
+sad_dir = os.path.join(base_dir, "sad/")
+
+print("Sample happy image:")
+plt.imshow(load_img(f"{os.path.join(happy_dir, os.listdir(happy_dir)[0])}"))
+plt.show()
+
+print("\nSample sad image:")
+plt.imshow(load_img(f"{os.path.join(sad_dir, os.listdir(sad_dir)[0])}"))
+plt.show()
+
+from tensorflow.keras.preprocessing.image import img_to_array
+
+# Load the first example of a happy face
+sample_image  = load_img(f"{os.path.join(happy_dir, os.listdir(happy_dir)[0])}")
+
+# Convert the image into its numpy array representation
+sample_array = img_to_array(sample_image)
+
+print(f"Each image has shape: {sample_array.shape}")
+
+print(f"The maximum pixel value used is: {np.max(sample_array)}")
+
+
+class myCallback(tf.keras.callbacks.Callback):
+    def on_epoch_end(self, epoch, logs={}):
+        if logs.get('accuracy') is not None and logs.get('accuracy') > 0.999:
+            print("\nReached 99.9% accuracy so cancelling training!")
+            self.model.stop_training = True
+
+
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
+
+
+def image_generator():
+    # Instantiate the ImageDataGenerator class.
+    # Remember to set the rescale argument.
+    train_datagen = ImageDataGenerator(rescale=1 / 255)
+
+    # Specify the method to load images from a directory and pass in the appropriate arguments:
+    # - directory: should be a relative path to the directory containing the data
+    # - targe_size: set this equal to the resolution of each image (excluding the color dimension)
+    # - batch_size: number of images the generator yields when asked for a next batch. Set this to 10.
+    # - class_mode: How the labels are represented. Should be one of "binary", "categorical" or "sparse".
+    #               Pick the one that better suits here given that the labels are going to be 1D binary labels.
+    train_generator = train_datagen.flow_from_directory(directory=base_dir,
+                                                        target_size=(150, 150),
+                                                        batch_size=10,
+                                                        class_mode='binary')
+    ### END CODE HERE
+
+    return train_generator
+# Save your generator in a variable
+gen = image_generator()
+from tensorflow.keras import optimizers, losses
+from tensorflow.keras.optimizers import RMSprop
 
 
 # GRADED FUNCTION: train_happy_sad_model
-def train_happy_sad_model():
-    # Please write your code only where you are indicated.
-    # please do not remove # model fitting inline comments.
-
-    DESIRED_ACCURACY = 0.999
-
-    class myCallback(tf.keras.callbacks.Callback):
-        # Your Code
-        def on_epoch_end(self, epoch, logs={}):
-            if (logs.get('acc') > DESIRED_ACCURACY):
-                print("\nReached 99.9% accuracy so cancelling training!")
-                self.model.stop_training = True
-
+def train_happy_sad_model(train_generator):
+    # Instantiate the callback
     callbacks = myCallback()
 
-    # This Code Block should Define and Compile the Model. Please assume the images are 150 X 150 in your implementation.
+    # Define the model
     model = tf.keras.models.Sequential([
         # Your Code Here
         tf.keras.layers.Conv2D(16, (3, 3), activation='relu', input_shape=(150, 150, 3)),
@@ -44,3 +83,26 @@ def train_happy_sad_model():
         tf.keras.layers.Dense(1, activation='sigmoid')
     ])
 
+    # Compile the model
+    # Select a loss function compatible with the last layer of your network
+    model.compile(loss='binary_crossentropy',
+                  optimizer=RMSprop(lr=0.001),
+                  metrics=['accuracy'])
+
+    # Train the model
+    # Your model should achieve the desired accuracy in less than 15 epochs.
+    # You can hardcode up to 20 epochs in the function below but the callback should trigger before 15.
+    history = model.fit(x=train_generator,
+                        epochs=15,
+                        callbacks=[callbacks]
+                        )
+
+    ### END CODE HERE
+    return history
+hist = train_happy_sad_model(gen)
+
+print(f"Your model reached the desired accuracy after {len(hist.epoch)} epochs")
+if not "accuracy" in hist.model.metrics_names:
+    print("Use 'accuracy' as metric when compiling your model.")
+else:
+    print("The metric was correctly defined.")
